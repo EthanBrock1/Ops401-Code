b6812a9364672e0e5d5b35dcd20ac0b4a9c07441
EthanBrock1 <ethancbrock13@gmail.com>
2023-04-19 01:41:43 +0000

    StartCode

---
+++ 401CC-02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Script: Ops 401 Class 02 Ops Challenge Solution
+# Author: Ethan Brock
+# Date of latest revision: 18APR2023
+# Purpose: Uptime Sensor Tool
+
+    #REQUIREMENTS
+# Transmit a single ICMP (ping) packet to a specific IP every two seconds.
+# Evaluate the response as either success or failure.
+# Assign success or failure to a status variable.
+# For every ICMP transmission attempted, print the status variable along with
+# a comprehensive timestamp and destination IP tested.
+    #DEMO
+
+
+    # Start Code
+
+    # End Code
