5a966e529cbea5ff9426876afb3c723cf49fdd64
EthanBrock1 <ethancbrock13@gmail.com>
2023-04-25 23:45:03 +0000

    code

--- 401CC-07.py
+++ 401CC-07.py
@@ -1,34 +1,20 @@
 #!/usr/bin/env python3
 
-# Script: Ops 401 Class 6 Ops Challenge
+# Script: Ops 401 Class 7 Ops Challenge Solution
 # Author:Ethan Brock
-# Date of latest revision: 24APR2023
+# Date of latest revision: 25APR2023
 # Purpose: 
 
     #REQUIREMENTS
-#In Python, create a script that utilizes the cryptography library to:
+#Add a feature capability to your script to:
 
-#Prompt the user to select a mode:
-#Encrypt a file (mode 1)
-#Decrypt a file (mode 2)
-#Encrypt a message (mode 3)
-#Decrypt a message (mode 4)
-#If mode 1 or 2 are selected, prompt the user to provide a filepath to a target file.
-#If mode 3 or 4 are selected, prompt the user to provide a cleartext string.
-#Depending on the selection, perform one of the below functions. You’ll need to create four functions:
-
-#Encrypt the target file if in mode 1.
-#Delete the existing target file and replace it entirely with the encrypted version.
-#Decrypt the target file if in mode 2.
-#Delete the encrypted target file and replace it entirely with the decrypted version.
-#Encrypt the string if in mode 3.
-#Print the ciphertext to the screen.
-#Decrypt the string if in mode 4.
-#Print the cleartext to the screen.
+#Recursively encrypt a single folder and all its contents.
+#Recursively decrypt a single folder that was encrypted by this tool.
     #DEMO
-# Help from Tyler and ChatGPT
+
 
     # Start Code
+import os
 from cryptography.fernet import Fernet
 
 # Declare Functions
@@ -41,23 +27,23 @@ def load_key():
     return open("key.key", "rb").read()
 
 def display_menu():
-    menu = ["1: Encrypt a file", "2: Decrypt a file", "3: Encrypt a message", "4: Decrypt a message", "5: Exit"]
+    menu = ["1: Encrypt a file", "2: Decrypt a file", "3: Encrypt a message", "4: Decrypt a message", "5: Encrypt a folder", "6: Decrypt a folder", "7: Exit"]
     for i in menu:
         print(i)
 
-def process_file(mode, file_name, key):
-    with open(file_name, "rb") as file:
+def process_file(mode, file_path, key):
+    with open(file_path, "rb") as file:
         file_data = file.read()
     fernet = Fernet(key)
 
     if mode == "1":
         encrypted = fernet.encrypt(file_data)
-        with open(file_name, "wb") as file:
+        with open(file_path, "wb") as file:
             file.write(encrypted)
         return encrypted
     elif mode == "2":
         decrypted = fernet.decrypt(file_data)
-        with open(file_name, "wb") as file:
+        with open(file_path, "wb") as file:
             file.write(decrypted)
         return decrypted
 
@@ -67,8 +53,12 @@ def process_message(mode, message, key):
         return fernet.encrypt(message.encode('utf-8'))
     elif mode == "4":
         return fernet.decrypt(message.encode('utf-8'))
-    
-    
+
+def process_folder(mode, folder_path, key):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            process_file(mode, file_path, key)
 
 # Generate and write a new key
 write_key()
@@ -81,8 +71,8 @@ while True:
     mode = input("Please select a mode: ")
 
     if mode in ["1", "2"]:
-        file_name = input("Enter file name: ")
-        processed_data = process_file(mode, file_name, key)
+        file_path = input("Enter file path: ")
+        processed_data = process_file(mode, file_path, key)
         if mode == "1":
             print("Encrypted message is: " + processed_data.decode('utf-8'))
         elif mode == "2":
@@ -95,5 +85,14 @@ while True:
         elif mode == "4":
             print("Decrypted message is: " + processed_data.decode('utf-8'))
     elif mode == "5":
+        folder_path = input("Enter folder path: ")
+        process_folder("1", folder_path, key)
+        print("Folder encrypted successfully!")
+    elif mode == "6":
+        folder_path = input("Enter folder path: ")
+        process_folder("2", folder_path, key)
+        print("Folder decrypted successfully!")
+    elif mode == "7":
         exit()
+
     # End Code
